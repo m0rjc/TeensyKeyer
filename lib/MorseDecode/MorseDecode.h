49da9de16de5689f1fc31df15f6c52e258af6876
Richard Corfield <Richard.Corfield@gmail.com>
2019-04-06 22:57:13 +0100

    Added documentation, checking also repository configuration

--- lib/MorseDecode/MorseDecode.h
+++ lib/MorseDecode/MorseDecode.h
@@ -1,3 +1,20 @@
+#ifndef __MORSE_DECODE_H__
+#define __MORSE_DECODE_H__
+
+/**
+ * Provide a state machine to identify individual Morse symbols.
+ * A Morse symbol lasts until an inter-character gap is read.
+ * 
+ * Once enough inter-character gaps are read a special SPACE symbol will be
+ * generated. This will only be generated once until new non-space data
+ * is received.
+ * 
+ * A convention in Morse Code is to indicate error by sending a stream of dits,
+ * perhaps by holding down the dit paddle on an electronic keyer. This code will
+ * generate the BACKSPACE special symbol for each 8 dits it receives and keep doing
+ * so until DIT is released. Any dits in the stream not a multiple of 8 will be
+ * discarded to prevent spurious E,I,S,H,5 being generated.
+ */
 namespace MorseDecode {
     typedef void (*symbolCallback)(unsigned short);
 
@@ -22,3 +39,5 @@ namespace MorseDecode {
      */
     extern void onCharacterGap();
 };
+
+#endif
