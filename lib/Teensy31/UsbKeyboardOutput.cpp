a5bc03d2096a3598ef0ac13797f866e6d630f411
Richard Corfield <Richard.Corfield@gmail.com>
2019-04-13 17:18:08 +0100

    First experiments with the decoder to serial port. Partially occupied symbol table.

--- lib/Teensy31/UsbKeyboardOutput.cpp
+++ lib/Teensy31/UsbKeyboardOutput.cpp
@@ -1,9 +1,61 @@
 #include "Teensy31Hardware.h"
+#include "MorseCharacters.h"
 
 #include <Keyboard.h>
 
 namespace Teensy31
 {
+
+class IKeyboardAction {
+    public:
+    virtual void act(void) = 0;
+};
+
+class CharacterKeyboardAction : public IKeyboardAction {
+    private:
+    char ch;
+    public:
+    CharacterKeyboardAction(char ch) : ch(ch) {}
+    void act(void) {
+        Serial.write(ch);
+    }
+};
+
+static char CHARACTERS[256] = {
+    /* 0  */ 0,
+    /* -  */ 't',
+    /* .- */ 'a',
+    /* -- */ 'm',
+    /* ..- */ 'u',
+    /* -.- */ 'k',
+    /* .-- */ 'w',
+    /* --- */ 'o',
+    /* ...- */ 'v',
+    /* -..- */ 'x',
+    /* .-.- */ 0,
+    /* --.- */ 'q',
+    /* ..-- */ 0,
+    /* -.-- */ 'y',
+    /* .--- */ 'j',
+    /* ---- */ 0,
+    /* ....- */ '4',
+    /* -...- */ '=',
+    /* .-..- */ 0,
+    /* --..- */ 0,
+    /* ..-.- */ 0,
+    /* -.-.- */ 0,
+    /* .--.- */ 0,
+    /* ---.- */ 0,
+    /* ...-- */ '3',
+    /* -..-- */ 0,
+    /* .-.-- */ 0,
+    /* --.-- */ 0,
+    /* ..--- */ '2',
+    /* -.--- */ 0,
+    /* .---- */ '1',
+    /* ----- */ '0',
+    /* .....- */ 0
+};
 
 UsbKeyboardOutput::UsbKeyboardOutput(void)
 {
@@ -12,5 +64,18 @@ UsbKeyboardOutput::UsbKeyboardOutput(void)
 
 void UsbKeyboardOutput::onSymbol(unsigned short symbol)
 {
+    if(symbol < 32)
+    {
+//        IKeyboardAction *action = ACTIONS[symbol]; 
+//        if(action) action->act();
+        char ch = CHARACTERS[symbol];
+        if(ch) Serial.write(ch);
+    }
+    else if(symbol == MORSE_BACKSPACE) {
+        Serial.printf("<BS>");
+    } else if (symbol == MORSE_SPACE) {
+        Serial.write(' ');
+    }
+//    Serial.printf("Symbol %04x\n", (int)symbol);
 }
 } // namespace Teensy31
